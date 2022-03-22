8dfec34a670f9cf5ef9a01bc964c0c8780016f97
Eleonora <eleonora.sturo@gmail.com>
2022-03-22 20:56:51 +0200

    Day 9 Exercise with mapping and filtering

---
+++ src/main/scala/Day9WordLengthExercise.scala
@@ -0,0 +1,29 @@
+import scala.io.StdIn.readLine
+
+object Day9WordLengthExercise extends App{
+  //TODO ask user to enter a sentence
+  //Split sentence into words using split , you will will have a sequence of words, we did this on Day 8
+  //Generate word length sequence (can use map or yield)
+  //Filter only words of length over 5
+  //print word lengths for every word
+  //print the long words
+
+  //you are allowed to use yield or map/filter
+
+  val sentence = readLine("Enter any sentence, please:\n")
+
+  val words = sentence.split(" ")
+  println(s"The words from sentence:\n${words.mkString(",")}")
+
+  val wordsLength = words.map(_.length)
+  println(s"Word lengths:\n${wordsLength.mkString(",")}")
+
+  val wordsOver5 = words.filter(it => it.length > 5)
+  println(s"Words of length over 5:\n${wordsOver5.mkString(",")}")
+
+  for (word <- words){
+    println(s"$word length is ${word.length}")
+  }
+
+
+}
