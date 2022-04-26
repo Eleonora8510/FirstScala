21bd110b9c8b454f30734177dd1ad721e8f9db2b
Eleonora <eleonora.sturo@gmail.com>
2022-04-26 20:53:43 +0300

    Fruit case class

---
+++ src/main/scala/com/github/Eleonora8510/Fruit.scala
@@ -0,0 +1,14 @@
+package com.github.Eleonora8510
+
+case class Fruit(genus: String,
+                 name: String,
+                 id: Int,
+                 family: String,
+                 order: String,
+                 carbohydrates: Double = 0.0,
+                 protein: Double = 0.0,
+                 fat: Double = 0.0,
+                 calories: Int = 0,
+                 sugar: Double = 0.0)
+
+
