3f60fc48f125a270e18418f6c16d5453b45df09e
Eleonora <eleonora.sturo@gmail.com>
2022-05-02 20:19:08 +0300

    Day 23 Solution

---
+++ src/resources/db/Day23solution.sql
@@ -0,0 +1,13 @@
+SELECT * FROM customers c
+WHERE Country != "USA";
+
+SELECT * FROM customers c 
+WHERE Country = "Germany";
+
+SELECT * FROM employees e
+WHERE Title LIKE "Sales % agent";
+
+SELECT DISTINCT BillingCountry FROM invoices;
+
+
+
