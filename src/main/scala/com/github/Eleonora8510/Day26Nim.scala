54344f5d83843419359d01cd114c30185576a611
Eleonora <eleonora.sturo@gmail.com>
2022-05-10 13:40:32 +0300

    Day 26 NIM working first version of the game

--- src/main/scala/com/github/Eleonora8510/Day26Nim.scala
+++ src/main/scala/com/github/Eleonora8510/Day26Nim.scala
@@ -27,11 +27,36 @@ object Day26Nim extends App{
 
   println(s"Palyer A - $playerA and Pplayer B - $playerB  let us play NIM")
 
+  // inevitably in most applications we will have some state that we want to keep track of (sledzic, sekti)
+  //here it is simple enough state that we can use a few variables
+  // at some point we will want to structure this game/app state into separate object based on some class
 
+  var currentState = startingCount
+  var isPlayerATurn = true // so A goes first
 
   // main loop - while there are some matches play on
-  // implement PvP (Player versus Player) - computer only checks the rules
-  // PvC - Player versus Computer - you will need to add some logic to the computer
+  // TODO implement PvP (Player versus Player) - computer only checks the rules
+  while (currentState > gameEndCondition){
+    val currentPlayer = if (isPlayerATurn) playerA else playerB
+    //show the game state
+    println(s"Currently there are $currentState matches on the table")
+    val move = readLine(s"How many matches do you want to take $currentPlayer? (1-3) ").toInt //TODO error checking
+    //TODO limit the number of matches according to the rules
+    currentState -= move
+    isPlayerATurn = !isPlayerATurn // toggle trick to change a boolean to reverse version of present
+    // play the game
+  }
+
+  // TODO PvC - Player versus Computer - you will need to add some logic to the computer
+
+  //end cleanup here we just print some game state and congratulations
+  //TODO add saving to Database, stats, etc
+
+  val winner = if (isPlayerATurn) playerA else playerB
+  val loser = if (!isPlayerATurn) playerA else playerB
+  println(s"Game ended. Congratulations $winner! Better luck next time $loser.")
+
+ // TODO implement multiple games
 
   // no clean up at the moment
 
