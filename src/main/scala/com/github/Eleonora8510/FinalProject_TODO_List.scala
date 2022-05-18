1f9d193e7feb2ad03dd4ee5c474edc2322aa954f
Eleonora <eleonora.sturo@gmail.com>
2022-05-18 22:48:34 +0300

    Task List Manager with main loop and connection to databse

--- src/main/scala/com/github/Eleonora8510/FinalProject_TODO_List.scala
+++ src/main/scala/com/github/Eleonora8510/FinalProject_TODO_List.scala
@@ -1,49 +1,76 @@
 package com.github.Eleonora8510
 
+import java.nio.file.{Files, Paths}
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import scala.io.StdIn.readLine
 
-class Tasks(id: Int,
-            task: String,
+class Tasks(task: String,
             created: String =  DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm").format(LocalDateTime.now),
             status: String,
             deadline: String) {
 
   def prettyPrint(): Unit = {
-    println(s"id: $id, task: $task, created: $created, status: $status, deadline: $deadline")
+    println(s"task: $task, created: $created, status: $status, deadline: $deadline")
   }
 }
 
 object FinalProject_TODO_List extends App{
   println("Hello!\nThis is Task List Manager")
+  var userIsActive = true
 
-  def enterNewTask():Unit = {
+  val dstFolder = "src/resources/todo"
+  Files.createDirectories(Paths.get(dstFolder))
+
+  // create DB
+  val db = new ToDoDB("src/resources/todo/todo.db")
+
+//  // create tables in DB
+  db.migrate()
+
+
+  /**
+   * return the prettyPrint of task inserted
+   */
+  def enterNewTask():(String, String, String) = {
     val task = readLine("Enter the task: ")
     val status = "created"
-    // TODO check if the deadline is not not earlier than current time
+    // TODO check if the deadline is not earlier than current time
     val deadline = readLine("Enter the deadline: (yyyy-MM-dd_HH:mm)")
 
-    //TODO looping id
-
-    val newTask = new Tasks(id = 1, task = task, status = status, deadline = deadline)
+    val newTask = new Tasks(task = task, status = status, deadline = deadline)
 
     newTask.prettyPrint()
+    (task, status, deadline)
 
   }
 
-  def updateTaskStatus(): Unit = {
+  /**
+   *
+   * @return updated status
+   */
+  def updateTaskStatus(): String = {
     val newStatus = readLine("Choose the status:\n" +
       "(C) created\n" +
       "(P) in progress\n" +
-      "(F) finished")
+      "(F) finished\n")
 
     var updateStatus = ""
 
     if (newStatus.toLowerCase.startsWith("c")) updateStatus = "created"
-    else if (function.toLowerCase.startsWith("p")) updateStatus = "in progress"
-    else if (function.toLowerCase.startsWith("f")) updateStatus = "finished"
+    else if (newStatus.toLowerCase.startsWith("p")) updateStatus = "in progress"
+    else updateStatus = "finished"
+    updateStatus
 
+
+  }
+
+  /**
+   * leave the main loop
+   */
+  def quit(): Unit = {
+    println("All is done! See you next time.")
+    userIsActive = false
 
   }
 
@@ -52,19 +79,39 @@ object FinalProject_TODO_List extends App{
   }
 
   //TODO add checking if there are any tasks inserted in database(table tasks)
-  val function = readLine("What do you want to do:\n" +
-    "(E) enter a new task\n" +
-    "(U) update status of existing\n" +
-    "(R) show remaining tasks\n" +
-    "(D) delete task\n" +
-    "(Q) quit\n")
+
+  /**
+   * main loop
+   */
+  def setup(): Unit = {
+    while (userIsActive) {
+      val function = readLine("What do you want to do (choose the appropriate letter):\n" +
+        "(E) enter a new task\n" +
+        "(U) update status of existing\n" +
+        "(R) show remaining tasks\n" +
+        "(D) delete task\n" +
+        "(Q) quit\n")
+
+      if (function.toLowerCase.startsWith("e")) {
+        val tasksToInsert = enterNewTask
+        db.insertTask(tasksToInsert._1, tasksToInsert._2, tasksToInsert._3)
+        userIsActive = true
+      }
+      else if (function.toLowerCase.startsWith("u")) {
+        val newStatus = updateTaskStatus
+        db.insertNewStatus(newStatus)
+        userIsActive = true
+      }
+      else {
+        quit
+        userIsActive = false
+      }
+    }
+}
 
 
-  if (function.toLowerCase.startsWith("e")) enterNewTask
-  else if (function.toLowerCase.startsWith("u")) updateTaskStatus
-  else if (function.toLowerCase.startsWith("r")) showRemainingTasks
-
-
+  setup()
+  db.conn.close()
 
 
 }
